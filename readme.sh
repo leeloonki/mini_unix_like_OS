d26642cfd5ef1b17817c00e7eee6e1b1a06811fc
leeloonki <917708562@qq.com>
2022-01-11 11:51:17 +0800

    git简单使用总结

--- readme.sh
+++ readme.sh
@@ -3,7 +3,7 @@
  # @Author: leeloonki
  # @Bilibili: 李景芳_
  # @Date: 2022-01-10 11:08:32
- # @LastEditTime: 2022-01-10 11:33:12
+ # @LastEditTime: 2022-01-11 11:50:39
  # @LastEditors: leeloonki
  # @Description: 
  # @FilePath: \code\readme.sh
@@ -28,3 +28,18 @@ cd  # 回到用户主目录
 source ./cd_codefolder.sh   # 切换到共享目录
 
 
+
+# 仓库管理
+git init                    # master
+git branch -M main          # 修改本地分支master->main
+git remote add origin git@github.com:leeloonki/OS_exercise.git #添加远程仓库
+git status
+git log
+git add .
+git status
+git commit -m "change "
+git push -u origin main
+git branch -a               # 查看所有分支
+# PS D:\vm_sharedfolder\code> git branch -a
+# * main                    # 本地main分支
+#   remotes/origin/main     # 远程main分支
