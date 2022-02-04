d6c297363a205f5c4a5ab2e64bc6904f415a0dde
leeloonki <917708562@qq.com>
2022-02-04 22:47:07 +0800

    实现printf boot文件夹存在问题

---
+++ 27_实现printf/kernel/global.h
@@ -0,0 +1,93 @@
+#ifndef __KERNEL_GLOBAL_H
+#define __KERNEL_GLOBAL_H
+      
+#include "stdint.h"
+/************************************ GDT描述符属性 *******************************/
+#define DESC_G_4K   1
+#define DESC_D_32   1
+#define DESC_L      0
+#define DESC_AVL    0
+#define DESC_P      1
+#define DESC_DPL_0  0
+#define DESC_DPL_3  3  //00 01 10 11
+#define DESC_S_CODE 1               //EXECUTABLE SEGMENT DESCRIPTOR  S位为1
+#define DESC_S_DATA DESC_S_CODE     //DATA SEGMENT DESCRIPTOR   S位为1
+#define DESC_S_SYS  0               //SYSTEM SEGMENT DESCRIPTOR S位为0
+// ; 0 E W A
+// ; E = 0 向上扩展
+// ; W = 1 数据段可写
+// ; A = 0 未访问过
+#define DESC_TYPE_DATA  2            //高4字节中7到11位type属性
+// ; 1 C R A
+// ; C = 0 非一致性
+// ; R = 0 只执行，不可读
+// ; A = 0 未访问过
+#define DESC_TYPE_CODE  8
+
+// TSS描述符中的B位是“忙”位（Busy）。在任务刚刚创建的时候，它应该为二进制的1001，即，B位是“0”，表明任务不忙。
+// 1001b = 9
+#define DESC_TYPE_TSS   9
+
+#define RPL0 0
+#define RPL3 3
+
+#define TI_GDT 0        
+#define TI_LDT 1  
+
+// 创建GDT选择子
+#define SELECTOR_K_CODE ((1<<3) + ( TI_GDT <<2 ) + RPL0 ) //内核代码段 GDT 第1项
+#define SELECTOR_K_DATA ((2<<3) + ( TI_GDT <<2 ) + RPL0 ) //内核数据段 GDT 第2项
+#define SELECTOR_K_STACK SELECTOR_K_DATA
+#define SELECTOR_K_VIDEO ((3<<3) + ( TI_GDT <<2 ) + RPL0 )//内核显存段 GDT 第3项
+#define SELECTOR_U_CODE ((5<<3) + ( TI_GDT <<2 ) + RPL3 ) //内核显存段 GDT 第5项
+#define SELECTOR_U_DATA ((6<<3) + ( TI_GDT <<2 ) + RPL3 ) //内核数据段 GDT 第6项
+#define SELECTOR_U_STACK SELECTOR_U_DATA                    
+
+// GDT属性位：高四字节的20-23位  代码段数据段该些位属性相同
+#define GDT_ATTR_HIGH ((DESC_G_4K << 7) + (DESC_D_32 << 6) + (DESC_L << 5 ) + (DESC_AVL << 4 ))
+// GDT属性位：高四字节的8-15位  特权级为3的代码段数据段的部分属性位
+#define GDT_CODE_ATTR_LOW_DPL3 ( (DESC_P << 7) + (DESC_DPL_3 << 5) + (DESC_S_CODE << 4) + DESC_TYPE_CODE ) 
+#define GDT_DATA_ATTR_LOW_DPL3 ( (DESC_P << 7) + (DESC_DPL_3 << 5) + (DESC_S_DATA << 4) + DESC_TYPE_DATA )
+
+/************************************ TSS描述符属性 *******************************/
+#define TSS_DESC_D 0
+#define SELECTOR_TSS ((4<<3) + ( TI_GDT <<2 ) + RPL0 )    //内核TSS段 GDT 第4项
+// TSS段描述符属性
+#define TSS_ATTR_HIGH ((DESC_G_4K <<7 ) + (TSS_DESC_D <<6) + (DESC_L << 5) + (DESC_AVL << 4) + 0x0) 
+#define TSS_ATTR_LOW ((DESC_G_4K <<7 ) + (DESC_DPL_0 <<5) + (DESC_S_SYS << 4) + DESC_TYPE_TSS ) 
+
+// GDT描述符结构体
+struct gdt_desc{
+    uint16_t limit_low_word;            //低四字节：0-15位段界限
+    uint16_t base_low_word;             //低四字节：16-31位段基址
+    uint8_t base_mid_byte;              //高四字节：0-7位段基址
+    uint8_t attr_low_byte;              //高四字节：8-15位属性低位
+    uint8_t limit_high_attr_high;       //高四字节：16-23位 偏移高位属性高位
+    uint8_t base_high_byte;             //高四字节：24-31位段基址高位
+};
+
+
+/************************************ IDT描述符属性 *******************************/
+#define IDT_DESC_P  1
+#define IDT_DESC_DPL0   0
+#define IDT_DESC_DPL3   3
+#define IDT_DESC_TYPE   0xE//1110
+
+#define IDT_DESC_ATTR_DPL0 (( IDT_DESC_P << 7 ) + ( IDT_DESC_DPL0 << 5 )+ IDT_DESC_TYPE )
+#define IDT_DESC_ATTR_DPL3 (( IDT_DESC_P << 7 ) + ( IDT_DESC_DPL3 << 5 )+ IDT_DESC_TYPE )
+
+/************************************ eflags属性 *******************************/
+#define EFLAGS_2 (1<<1)                 //第2位固定为1
+#define EFLAGS_IF_1 (1<<9)
+#define EFLAGS_IF_0 (0<<9)
+#define EFLAGS_IOPL_3 (3<<12)           //IOPL3测试用户程序在非系统调用下进程IO
+#define EFLAGS_IOPL_0 (0<<12)
+
+#define DIV_ROUND_UP(X, STEP) ((X + STEP - 1) / (STEP))
+/*数据类型*/
+#define NULL ((void*)0)
+#define bool int
+#define true 1
+#define false 0
+#define PG_SIZE 4096
+#endif
---
+++ 27_实现printf/lib/stdio.c
@@ -0,0 +1,64 @@
+#include "stdio.h"
+#include "global.h"
+#include "stdint.h"
+#include "string.h"
+#include "syscall.h"
+
+
+#define va_start(ap,last) ap=(va_list)&last     //format为char*类型，将其转化为为va_list获取format参数地址
+#define va_arg(ap,type) *((type*)(ap += 4))         //ap指向下一个参数，并返回其值
+#define va_end(ap) ap=NULL
+
+
+// printf("The raw value: i=%d\n", i)
+
+// 将整型转换为字符型
+// int -> asci
+// 参数1：待转换整型，参数2：转换后保存ASCII的缓冲区地址的地址，参数3：进制
+static void itoa(uint32_t value,char** buf_ptr_addr,uint8_t base){
+    uint32_t m = value % base;  //求模
+    uint32_t i = value / base;  //取整  30%8=6 30/8=3 3%8=3 3/8=0  36
+    if(i){ //如果取整后不为0
+        itoa(i,buf_ptr_addr,base);
+    }
+    if(m<10){ //如果本次余数为0-9，最先执行本语句的为递归调用的最后一轮，得到的为数字value的最高位
+        *((*buf_ptr_addr)++) = m + '0';     //将数转化为字符，即buf_ptr_addr开始从高位写入
+    }else{ //余数A-F
+        *((*buf_ptr_addr)++) = m - 10 + 'A'; //-10后相对字符A的值
+    }  
+}
+
+// 将参数指针ap指向的参数按照格式format输出到字符串str,并返回str长度
+uint32_t vsprintf(char *str, const char *format, va_list ap){
+    char* buf_ptr = str;                //buf_str指向存放格式化后字符串的缓冲区
+    const char* index_ptr = format;     
+    char index_char = *index_ptr;
+    int32_t arg_int;                    
+    while(index_char){
+        if(index_char!='%'){
+            *(buf_ptr++) = index_char;  //将format字符串中非%类型字符的字符拷贝到str
+            index_char = *(++index_ptr);
+            continue;
+        }
+        // 如果本次为'%'
+        index_char = *(++index_ptr);    //获取%后的类型字符如%d %f %x 的d f x
+        switch(index_char){
+            case 'x':                   //x十六进制
+            arg_int = va_arg(ap,int);   //va_arg使用前必须调用va_start初始化ap指向第一个参数format的地址
+            itoa(arg_int,&buf_ptr,16);  //调用完成后buf_ptr的"值"会++,即指向缓冲区的下一个地址
+            index_char = *(++index_ptr);
+            break;
+        }
+    }
+    return strlen(str);
+}
+
+// 格式化输出字符串
+uint32_t printf(const char* format,...){
+    va_list args;               //args 即ap
+    va_start(args,format);      //args指向format
+    char buf[1024] = {0};
+    vsprintf(buf,format,args);
+    va_end(args);
+    return write(buf);
+}
